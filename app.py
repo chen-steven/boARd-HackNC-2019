460ae07d1318c1219fb69205666101b80e8dc5b3
Mariusz Derezinski-Choo <mariusz.derezinskichoo@duke.edu>
2019-10-12 16:25:41 -0400

    fixed

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+app.config.update(
+    SECRET_KEY = 'public static void main(String[] args)'
+)
+
+
+@app.route("/Hello_World", methods = ['POST'])
+def test():
+    return jsonify("Hello World!")
+
