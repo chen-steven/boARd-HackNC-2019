30498cd5b925a183f72951cc6c39c49959e408d3
Mariusz Derezinski-Choo <mariusz.derezinskichoo@duke.edu>
2019-10-13 08:34:21 -0400

    final

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def room():
 @app.route("/TEXT", methods = ['GET'])
 def getText():
     iden = request.args.get('id')
+    print(inden)
     group = Room.query.filter_by(id = int(iden)).first()
     group = group.__repr__()
     payload = {'room' : str(group[0]),
